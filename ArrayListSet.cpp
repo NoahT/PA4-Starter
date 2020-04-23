81f6492eedfd75c3b1c651a15e7feb9d4f700d72
Noah Teshima <noah.teshima@gmail.com>
2020-04-23 03:38:37 +0000

    ArrayList implementation

--- ArrayListSet.cpp
+++ ArrayListSet.cpp
@@ -4,17 +4,37 @@
  * Implement the ArrayListSet methods correctly
  */
 unsigned int ArrayListSet::size() {
-    /* YOUR CODE HERE */
+    return this->arr.size();
 }
 
 void ArrayListSet::insert(string s) {
-    /* YOUR CODE HERE */
+    if(!this->find(s)) {
+        this->arr.push_back(s);
+    }
 }
 
 void ArrayListSet::remove(const string & s) {
-    /* YOUR CODE HERE */
+    vector<string>::iterator iter = this->arr.begin();
+    
+    // sets do not have duplicate elements. IF we find the
+    // element we are done iterating.
+    while(iter != this->arr.end()) {
+        if(*iter == s) {
+            this->arr.erase(iter);
+            break;
+        }
+    }
 }
 
 bool ArrayListSet::find(const string & s) {
-    /* YOUR CODE HERE */
+    vector<string>::iterator iter = this->arr.begin();
+
+    while(iter != this->arr.end()) {
+        if(*iter == s) {
+            return true;
+        }
+        iter++;
+    }
+
+    return false;
 }
